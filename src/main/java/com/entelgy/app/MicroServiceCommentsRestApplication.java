3b2b0ec022c91bb3c4e0033c51095c3d1c41d01a
Kleiber Maluquish <maluquish.19@gmail.com>
2021-02-27 05:18:36 -0500

    Proyecto base Comments

---
+++ src/main/java/com/entelgy/app/MicroServiceCommentsRestApplication.java
@@ -0,0 +1,13 @@
+package com.entelgy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceCommentsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceCommentsRestApplication.class, args);
+	}
+
+}
